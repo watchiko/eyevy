b256bf5e939d7870c545a6a2c5ec64f4a0f17fde
Maneesh <mnsh.exe@gmail.com>
2025-10-31 02:06:05 +0530

    fixed overflowing overall width

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header/Header";
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <div className="min-w-screen min-h-screen bg-white flex flex-col">
+      <div className="min-h-screen bg-white flex flex-col">
         <div className="flex-1">
           <Header />
           {children}
--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -3,7 +3,7 @@ import HomeHeader from "@/components/Header/HomeHeader";
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <div className="min-w-screen min-h-screen bg-white flex flex-col">
+      <div className="min-h-screen bg-white flex flex-col">
         <div className="flex-1">
           <HomeHeader />
           {children}
